0e2d4f6fe80773f1da5b62532469aa5d13d84841
ismail <ismailhsan45@gmail.com>
2021-09-01 23:47:08 +0600

    commit

--- controllers/product.js
+++ controllers/product.js
@@ -4,18 +4,18 @@ module.exports.getallproducts = async (req, res) => {
     const getquery = 'SELECT * FROM products;';
     await db.query(getquery, (err, data) => {
         if (err) {
-            res.json({ err });
+            res.json(err);
         }
-        res.json({ data });
+        res.json(data);
     })
 }
 module.exports.addproducts = async (req, res) => {
-    const { productName, productPrice, productDescription, url } = req.body;
-    const getquery = `INSERT INTO products (productname,productprice,productdetails,productimg) VALUES (?,?,?,?);`;
-    await db.query(getquery, [productName, productPrice, productDescription, url], (err, result) => {
+    const { productName, productPrice, productDescription, productFeatures, url, type } = req.body;
+    const getquery = `INSERT INTO products (productname,productprice,productdetails, productfeatures,productimg,type) VALUES (?,?,?,?,?,?);`;
+    await db.query(getquery, [productName, productPrice, productDescription, productFeatures, url, type], (err, result) => {
         if (err) {
             res.json({ err });
         }
-        res.json({ result });
+        res.json(result);
     })
 }
